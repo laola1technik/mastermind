0cb36998c399a5e998870a9d4a32b77ba2dad5c3
Karl Zierhofer <karl.zierhofer@laola1.at>
2019-01-11 10:39:18 +0100

    Uses parameterized tests with name.

--- Mastermind/app/src/test/java/at/weblaola1/dev/mastermind/MastermindTest.java
+++ Mastermind/app/src/test/java/at/weblaola1/dev/mastermind/MastermindTest.java
@@ -3,21 +3,51 @@ package at.weblaola1.dev.mastermind;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import java.util.Arrays;
 
-@RunWith(DataProviderRunner.class)
+@RunWith(Parameterized.class)
 public class MastermindTest {
 
+    private final Code correctCode;
+    private final Code guessedCode;
+    private final int wellPlaced;
+    private final int misplaced;
+
+    public MastermindTest(String testName, Code correctCode, Code guessedCode, int wellPlaced, int misplaced) {
+        this.correctCode = correctCode;
+        this.guessedCode = guessedCode;
+        this.wellPlaced = wellPlaced;
+        this.misplaced = misplaced;
+    }
+
     @Test
-    @UseDataProvider("correct_code_guessed_code_well_placed_misplaced")
-    public void should_give_well_placed_and_misplaced_for_guessed_code(Code correctCode, Code guessedCode, int wellPlaced, int misplaced) {
+    public void should_give_well_placed_and_misplaced_for_guessed_code() {
         CompareResult result = correctCode.compareWith(guessedCode);
 
         Assert.assertEquals(wellPlaced, result.getNumberOfWellPlaced());
         Assert.assertEquals(misplaced, result.getNumberOfMisplaced());
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {
+                        "should_return_one_wellplaced_and_zero_misplaced_for_code_red_and_guess_red",
+                        createCode(CodePeg.Type.RED),
+                        createCode(CodePeg.Type.RED),
+                        1,
+                        0
+                },
+                {
+                        "should_return_zero_well_placed_and_zero_misplaced_key_pegs_for_code_red_and_guess_green",
+                        createCode(CodePeg.Type.RED),
+                        createCode(CodePeg.Type.GREEN),
+                        0,
+                        0
+                }
+        });
     }
 
     private static Code createCode(CodePeg.Type... codePegTypes) {
@@ -27,24 +57,4 @@ public class MastermindTest {
         }
         return new Code(codePegs);
     }
-
-    @DataProvider
-    public static Object[][] correct_code_guessed_code_well_placed_misplaced() {
-        return new Object[][]{
-                {
-                        createCode(CodePeg.Type.RED),
-                        createCode(CodePeg.Type.RED),
-                        1,
-                        0
-                },
-                {
-                        createCode(CodePeg.Type.RED),
-                        createCode(CodePeg.Type.GREEN),
-                        0,
-                        0
-                }
-
-        };
-    }
-
 }
