a78479477bef722873b69df58d1f5fa226723200
Karl Zierhofer <karl.zierhofer@laola1.at>
2019-08-22 14:55:08 +0200

    Adds game test. WIP

---
+++ Mastermind/app/src/test/java/at/weblaola1/dev/mastermind/GameTest.java
@@ -0,0 +1,19 @@
+package at.weblaola1.dev.mastermind;
+
+public class GameTest {
+
+    /*
+    public void should_get_well_places_pegs() {
+
+        HashSet<CodePeg> pegs = new HashSet<>(Arrays.asList(CodePeg.values()));
+        CodeGenerator codeGenerator = new CodeGenerator(pegs);
+        CodeMaker codeMaker = new CodeMaker();
+        CodeBreaker codeBreaker = new CodeBreaker();
+        GameSettings settings = new GameSettings(4, pegs);
+        Game game = new Game(codeMaker, codeBreaker,settings);
+        CompareResult result = game.getWellPlaced();
+
+    }
+     */
+
+}
